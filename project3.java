65d5a716e785deaeaf8209c2512e8543ae9eae7f
zainabkhalil08 <zainab08khalil@gmail.com>
2026-08-01 02:21:22 +0500

    error fixed

--- project3.java
+++ project3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 
-public class project2 {
+public class project3 {
 
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 100;
